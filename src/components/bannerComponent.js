0f21a8347ec2a47ac7f3b62b221a939445cae239
sheenxavi004 <sheenxavi004@gmail.com>
2020-11-29 19:26:16 +0530

    Adding Window Resizes

--- src/components/bannerComponent.js
+++ src/components/bannerComponent.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Banner(){
 
+    const [position, setPosition] = useState('center');
+    function setAlignment()
+    {
+        if(window.innerWidth <= 450)
+            setPosition('start');
+        else
+            setPosition('center');
+    }
+    window.addEventListener('resize', setAlignment);
     return(
     <div id="banner">
         <div className="container" id="event-heading">
-            <div className="row row-header align-items-center justify-content-center">
+            <div className={`row row-header align-items-${position}`}>
                 <div className="col" id="banner-heading">
                     <p>Welcome to IET On Campus CET</p>
                     <div id="explore">
